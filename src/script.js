f56ba5850cde236c0c45fb571937a268a76f466f
Elthon <eltonsilva.mecatronic@gmail.com>
2023-08-30 22:30:46 -0300

    criando arquivos e estilizacao

---
+++ src/script.js
@@ -0,0 +1,17 @@
+
+class CarrosDisponiveis{
+
+    constructor() {
+
+    }
+
+    adicionar() {
+        alert("Adicionando carro");
+    }
+
+    deletar() {
+        alert("Excluindo carro");
+    }
+}
+
+let carros = new CarrosDisponiveis();
